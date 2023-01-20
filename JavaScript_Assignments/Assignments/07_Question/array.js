e3fdf269a6882e11b330008bd65bb9280bf22911
Rohan <rohan.sambhudas@gmail.com>
2023-01-20 16:55:17 +0530

    Q7 Completed

---
+++ JavaScript_Assignments/Assignments/07_Question/array.js
@@ -0,0 +1,6 @@
+// Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+let array_list = ['Google', 'Microsoft', 'pw skills', 'Facebook', 'WhatsApps', 'pw skills'];
+
+console.log(array_list.indexOf('pw skills'));
+console.log(array_list.lastIndexOf('pw skills'));
