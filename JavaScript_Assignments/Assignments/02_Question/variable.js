96127719571cdbc487f68fb7409ee48639e43842
Rohan <rohan.sambhudas@gmail.com>
2023-01-20 11:31:37 +0530

    Files Name Updated

---
+++ JavaScript_Assignments/Assignments/02_Question/variable.js
@@ -0,0 +1,7 @@
+let first_name = "Rohan";
+let last_name = "Sambhudas";
+let marital_status = "Unmarried";
+let country = "India";
+let age = "23";
+
+console.log("My name is ", first_name, last_name, "I am", marital_status, "I am from", country, "My age is", age);
