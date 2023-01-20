54d0df616d98288845cb3965204787566a5b4df2
Rohan <rohan.sambhudas@gmail.com>
2023-01-20 17:02:34 +0530

    Q9 Completed

---
+++ JavaScript_Assignments/Assignments/09_Question/Boolean.js
@@ -0,0 +1,9 @@
+// Boolean value is either true or false.
+//     - Write three JavaScript statement example which provide truthy value.
+//     - Write three JavaScript statement example which provide falsy value.
+
+let happy = true;
+let sad = false;
+
+console.log("I am feeling very happy", happy);
+console.log("I am feeling very sad", sad);
