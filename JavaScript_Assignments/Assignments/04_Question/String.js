9ed52be2de6664cefbfdd1da7b92de1d2aef3e19
Rohan <rohan.sambhudas@gmail.com>
2023-01-20 15:19:36 +0530

    Q4 Completed

---
+++ JavaScript_Assignments/Assignments/04_Question/String.js
@@ -0,0 +1,8 @@
+let state = "Maharashtra";
+let check = "Rohan"
+
+if (state.includes(check)){
+    console.log("Maharashtra contains", check)
+}else{
+    console.log("Maharashtra not contains", check)
+}
