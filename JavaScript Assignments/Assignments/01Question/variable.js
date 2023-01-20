1649f8e69721c17c8a7ab50e35390619eb3beee7
Rohan <rohan.sambhudas@gmail.com>
2023-01-20 10:45:14 +0530

    Q1 Completed

---
+++ JavaScript Assignments/Assignments/01Question/variable.js
@@ -0,0 +1,31 @@
+// String
+let first_name = "Rohan";
+let sarname = 'Sambhudas';
+// print the variable value in console
+console.log("My name is", first_name , sarname);
+console.log("Data type of first name", typeof(first_name));
+console.log("Data type of sarname", typeof(sarname));
+
+// Boolean
+let educated = true;
+let married = false;
+// print the variable value in console
+console.log("I am educated", educated);
+console.log("I am married", married)
+console.log("Data type of educated", typeof(educated));
+console.log("Data type of married", typeof(married));
+
+// Number
+let age = 7;
+console.log("My age is", age);
+console.log("Data type of age", typeof(age));
+
+// Undefined
+let interest = undefined;
+console.log("My interest is", interest);
+console.log("Data type of interest", typeof(interest));
+
+// Null
+let hobby = null;
+console.log("My hobbies are", hobby);
+console.log("Data type of hobby", typeof(hobby));
