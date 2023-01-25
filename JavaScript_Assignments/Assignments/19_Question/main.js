21ba8a7660ad1d438ed8512bcf0b89b017d0a4ea
Rohan <rohan.sambhudas@gmail.com>
2023-01-25 09:45:44 +0530

    Q19 Added

---
+++ JavaScript_Assignments/Assignments/19_Question/main.js
@@ -0,0 +1,6 @@
+// Create a countries.js file and store the countries name into this file, create a file web_techs.js and store the popular web technology names into this file. Access both file in a third file named main.js
+import {countries_name} from "./countries";
+import {web_tech} from "./web_teches";
+
+console.log(countries_name);
+console.log(web_tech);
---
+++ JavaScript_Assignments/Assignments/19_Question/web_teches.js
@@ -0,0 +1,6 @@
+// Create a countries.js file and store the countries name into this file, create a file web_techs.js and store the popular web technology names into this file. Access both file in a third file named main.js
+
+let web_tech = ["HTML", "CSS", "JavaScript", "React JS", "Angular JS", "Vue JS", "TypeScript", "Bootstrap", "Backbone JS"];
+
+// export{web_tech};
+export default(web_tech);
